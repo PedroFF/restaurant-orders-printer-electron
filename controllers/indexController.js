30ef97c6ccc3b33c2fe0a476fd1aa4cf52b0efcc
Pedro Freitas <soufreitas.pedro@gmail.com>
2020-08-20 23:10:27 -0300

    Ajustando funções

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,57 +1,60 @@
-const axios = require('axios');
-const api_restaurant_url = require('../config.json').API_URL;
-const api_key = require('../config.json').API_KEY;
-const agenda = new Agenda();
-const configRestaurant = {
-    headers: {
-        'Authorization': AUTH_TOKEN,
-        'Accept': 'application/json',
-        'Glf-Api-Version': 2
-    }
-};
+$(document).ready(function () {
+    const axios = require('axios');
+    const api_restaurant_url = require('../config.json').API_URL;
+    const api_key = require('../config.json').API_KEY;
+    const agenda = require('node-cron');
+    const configRestaurant = {
+        headers: {
+            'Authorization': 'AUTH_TOKEN',
+            'Accept': 'application/json',
+            'Glf-Api-Version': 2
+        }
+    };
 
-const configAPI = {
-    headers: {
-        'x-access-token': API_KEY,
-    }
-};
+    const configAPI = {
+        headers: {
+            'x-access-token': 'API_KEY',
+        }
+    };
 
-let orders;
+    let orders;
 
-agenda.define('searchOrders', job => {
-    axios.post(
-        api_restaurant_url,
-        configRestaurant
-    ).then((newOrders) => {
-        printOrders(newOrders);
-        orders.push(newOrders);
-        //sendToAPI(newOrders);
-        
-    })
-        .catch(console.log('Nenhum pedido novo'));
-});
-agenda.every('15 seconds', 'searchOrders');
-
-function sendToAPI(orders) {
-    verifyKey(orders[0]);
-    foreach(order in orders) {
+    var task = agenda.schedule('*/15 * * * * *', () => {
         axios.post(
-            '/api/restaurants/orders',
-            order,
-            configAPI);
-    }
-}
-
-function verifyKey(order) {
-    if (api_key === null || undefined) {
-        axios.post('api/restaurants/signup',
-            data: {
-            name: order.resraurant_name,
-            key: order.restaurant_key,
-            systemToken: order.restaurant_token
+            api_restaurant_url,
+            configRestaurant
+        ).then((newOrders) => {
+            printOrders(newOrders);
+            orders.push(newOrders);
+            sendToAPI(newOrders);
+        }).catch((error) =>{
+            console.log(error.toJSON());
         });
+    });
+    task.start();
+
+    function sendToAPI(orders) {
+        verifyKey(orders[0]);
+        foreach(order in orders)
+        {
+            axios.post(
+                '/api/restaurants/orders',
+                order,
+                configAPI)
+                .then()
+                .catch();
+        }
     }
-}
+
+    function verifyKey(order) {
+        if (!api_key) {
+            axios.post('api/restaurants/signup',
+                {
+                name: order.resraurant_name, key: order.restaurant_key, systemToken: order.restaurant_token
+            }
+            ).catch(console.log('tomar no cu2'));
+        }
+    }
 
 function printOrders(orders) {
     createPrintHTML();
@@ -59,15 +62,15 @@ function printOrders(orders) {
     const fs = require('fs');
     let rawdata = fs.readFileSync('../restaurant-orders-printer-electron/printconfig.json');
     let options = JSON.parse(rawdata);
-    
-    let win = BrowserWindow.getFocusedWindow(); 
-	//let win = BrowserWindow.getAllWindows()[0]; 
 
-     win.webContents.print(options, (success, failureReason) => { 
-         if (!success) console.log(failureReason); 
- 
-         console.log('Impressão Iniciada'); 
-     }); 
+    let win = BrowserWindow.getFocusedWindow();
+	//let win = BrowserWindow.getAllWindows()[0];
+
+     win.webContents.print(options, (success, failureReason) => {
+         if (!success) console.log(failureReason);
+
+         console.log('Impressão Iniciada');
+     });
 
 }
 
