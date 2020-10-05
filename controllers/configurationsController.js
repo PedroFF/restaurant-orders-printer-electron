f34b2c5c3b1f454eae3465e52d92c9acc2716fb7
Pedro Freitas <soufreitas.pedro@gmail.com>
2020-10-04 22:16:39 -0300

    fontes diferentes

--- controllers/configurationsController.js
+++ controllers/configurationsController.js
@@ -4,18 +4,30 @@ const app = require('electron').remote.app;
 $(document).ready(function () {
     const webContents = remote.getCurrentWebContents();
     const printers = webContents.getPrinters();
+    const fonts = {0 : {displayName: "Arial", familyName: " \"Arial\", Helvetica, sans-serif"},
+        1: {displayName: "Courier New", familyName: "\"Courier New\", Courier, monospace"},
+        2: {displayName: "Lucida Console", familyName: "\"Lucida Console\", Monaco, monospace"},
+        3: {displayName: "Times New Roman", familyName: "\"Times New Roman\", Times, serif"}
+    };
+    console.log(fonts);
     console.log(printers);
     $.each(printers, function (id, valor) {
         $('#impressora').append($("<option></option>").attr("value", valor.displayName).text(valor.displayName));
     });
+    $.each(fonts, function (id, valor) {
+        $('#fonte').append($("<option></option>").attr("value", valor.displayName).text(valor.displayName));
+    });
     carregaConfig()
+    console.log('salvar', document.getElementById('fonte'))
 })
 
 function carregaConfig() {
     const fs = require('fs');
     let rawdata = fs.readFileSync(path.join(__dirname, '..', 'printconfig.json'));
     let options = JSON.parse(rawdata);
+    console.log('options --->', options);
     $("#impressora").val(options.deviceName).change()
+    $("#fonte").val(options.font.displayName).change()
     $('#vias').val(options.copies)
     let buffer = fs.readFileSync(path.join(__dirname, '..', 'config.json'));
     let config = JSON.parse(buffer);
@@ -29,6 +41,7 @@ function salvarConfig() {
         silent: 'true',
         printBackground: 'true',
         deviceName: document.getElementById('impressora').options[document.getElementById('impressora').selectedIndex].text,
+        font: document.getElementById('fonte').options[document.getElementById('impressora').selectedIndex].familyName,
         landscape: 'false',
         pagesPerSheet: 1,
         collate: false,
